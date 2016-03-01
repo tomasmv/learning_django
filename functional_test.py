3d873a3ff2064d786af5cc35f92722574a9e05e6
tmorenovasquez <tmorenovasquez@cogolabs.com>
2016-03-01 16:19:13 -0500

    functional test tests for input

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVistorTest(unittest.TestCase):
@@ -15,6 +16,16 @@ class NewVistorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+        head_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_Tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
