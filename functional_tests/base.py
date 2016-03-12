691f3c14e2b265f7eae6b5feae54001bb116ee9e
tmorenovasquez <tmorenovasquez@cogolabs.com>
2016-03-12 12:03:32 -0500

    rename all item input ids and names. still broken

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -5,6 +5,10 @@ import sys
 
 class FunctionalTest(StaticLiveServerTestCase):
 
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
+
+
     @classmethod
     def setUpClass(cls):
         for arg in sys.argv:
@@ -14,17 +18,21 @@ class FunctionalTest(StaticLiveServerTestCase):
         super().setUpClass()
         cls.server_url = cls.live_server_url
 
+
     @classmethod
     def tearDownClass(cls):
         if cls.server_url == cls.live_server_url:
             super().tearDownClass()
 
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
+
     def tearDown(self):
         self.browser.quit()
+
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
